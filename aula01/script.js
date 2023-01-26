1528442f1d1edbfa2b47c268da07e748a362101f
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2023-01-26 19:22:50 -0300

    Update a01.1
    - Add aula01
    - Add index.html
    - Add script.js

---
+++ aula01/script.js
@@ -0,0 +1,87 @@
+//console.log('Hello World!')
+
+//alert('Hello World!')
+
+/* var txt = document.getElementById('txt')
+
+txt.innerHTML = 'Hello World!'
+
+txt.addEventListener('click', () => {
+    txt.style.color = 'green'
+}) */
+
+/*
+strings - armazenam qualquer valor entre aspas
+numérica - armazena um número
+booleana - true or false
+*/
+
+/* var nome = "estevan" // string
+var num = 11 // numérica
+var bool = true // booleana */
+
+/* 
+var
+const 
+let
+*/
+
+// var
+/* var nome = 'estevan'
+
+console.log(nome)
+
+nome = 'joao'
+
+console.log(nome)
+*/
+
+// const
+/*const nome = 'estevan'
+
+console.log(nome) */
+
+// let
+
+
+/* function txt() {
+    let nome = 'estevan'
+
+    console.log(nome)
+}
+
+txt()
+
+let nome = 'joao'
+
+console.log(nome) */
+
+/* 
+Escopo global
+Escopo local/função
+Escopo de bloco
+*/
+
+/*
+// Escopo global
+
+var nome = 'Douglas'
+
+function click() {
+    alert(nome)
+}
+
+// Escopo local/função
+
+function click() {
+    var nome = 'Douglas'
+    alert(nome)
+}
+
+// Escopo de Bloco
+
+function click() {
+{var nome = 'Douglas'
+alert(nome)
+}}
+*/
